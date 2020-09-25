c8d401c17d651d2c3532cde0020c9654e26a3d34
Anay Pareek <59292344+TheLastAirbendr@users.noreply.github.com>
2020-09-26 02:37:47 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,74 +1,19 @@
 #!/bin/bash
 
-echologs()
-{
-	echo $(date +"%d/%m/%Y %X: ") "$@" >> logs.log
-}
+for file in setpass start-server
+	chmod u+x ${file}.sh &&
+	echo "Successfully made file ${file}.sh executable" ||
+	echo "Failed to make file ${file}.sh executable"
+	mv ${file}.sh $HOME/../usr/bin/${file} &&
+	echo "Successfully moved and file ${file} to /usr/bin" ||
+	echo "Failed to move file ${file} to /usr/bin"
 
-echo -n "" > logs.log
-
-for name in setpass start-server start-client
-do
-	chmod u+x "${name}.sh" &&
-	echologs "Made '${name}.sh' executable successfully" ||
-	echologs "Failed to make '${name}.sh' executable (code:${?})"
-
-	echologs "Checking to see if alias ${name} exists..."
-	response=$(grep "${name}=" ~/../usr/etc/bash.bashrc)
-	if [ ! -n "$response" ]
-	then
-		echologs "Alias '${name}' does not exist"
-		echologs "Creating alias '${name}'..."
-		echo -e "alias ${name}=\"~/${name}.sh\"" >> ~/../usr/etc/bash.bashrc &&
-		echologs "Alias '${name}' created successfully" ||
-		echologs "Failed to create alias '${name}' (code:${?})"
-	else
-		echologs "Alias '${name}' already exists"
-	fi
-done
-
-echologs "Checking to see if package 'MariaDB' is installed..."
-
-dpkg -s mariadb &> /dev/null
-if [ $? -eq 1 ]
-then
-        echologs "Package 'MariaDB' is not installed"
-	echologs "Installing..."
-        pkg install mariadb
-
-        code=$?
-
-        if [ $code -eq 100 ]
-        then
-                echologs "Could not locate package 'MariaDB'"
-                echologs "Failed to install package 'MariaDB'"
-        elif [ $code -eq 0 ]
-        then
-                dpkg -s mariadb &> /dev/null
-                if [ $? -eq 0 ]
-                then
-                        echologs "Installation complete"
-			~/start-server.sh &&
-			echologs "MySQL server started successfully" ||
-			echologs "MySQL server failed to start (code:${?})"
-                else
-			echologs "Package installation command executed but package 'MariaDB' was not installed"
-                fi
-        else
-                echologs "Some unkown error occured during installation (code:${code})"
-        fi
-
-else
-        echologs "Package 'MariaDB' is already installed"
-	~/start-server.sh &&
-	echologs "MySQL server started successfully" ||
-	echologs "MySQL server failed to start (code:${?})"
-fi
+echo -e "\nalias mysql=\"mysql -u root -h localhost -P 3306 -p\"\n\nstart-server" >> $HOME/../usr/etc/bash.bashrc &&
+echo "Successfully added commands to bash.bashrc" ||
+echo "Failed to add commands to bash.bashrc"
 
 pkg clean
 
 termux-wake-lock &&
-echologs "Acquired wakelock" ||
-echologs "Failed to acquire wakelock (code:${?})"
-
-echo "MySQL installed successfully"
+echo "Successfully acquired wake-lock" ||
+echo "Failed to acquire wake-lock"
