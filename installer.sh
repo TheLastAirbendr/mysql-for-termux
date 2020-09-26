9c2f28c664e9b6debce1f861d051e278b10c226f
Anay Pareek <59292344+TheLastAirbendr@users.noreply.github.com>
2020-09-27 00:03:21 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
+yes | pkg install mariadb
+
 for file in setpass start-server
-	chmod u+x ${file}.sh &&
-	echo "Successfully made file ${file}.sh executable" ||
-	echo "Failed to make file ${file}.sh executable"
-	mv ${file}.sh $HOME/../usr/bin/${file} &&
-	echo "Successfully moved and file ${file} to /usr/bin" ||
-	echo "Failed to move file ${file} to /usr/bin"
+do
+	chmod u+x ${file}.sh
+	mv ${file}.sh $HOME/../usr/bin/${file}
+done
 
-echo -e "\nalias mysql=\"mysql -u root -h localhost -P 3306 -p\"\n\nstart-server" >> $HOME/../usr/etc/bash.bashrc &&
-echo "Successfully added commands to bash.bashrc" ||
-echo "Failed to add commands to bash.bashrc"
-
+echo -e "\nalias mysql=\"mysql -u root -h localhost -P 3306 -p\"\n\nstart-server" >> $HOME/../usr/etc/bash.bashrc
 pkg clean
+start-server
 
-termux-wake-lock &&
-echo "Successfully acquired wake-lock" ||
-echo "Failed to acquire wake-lock"
